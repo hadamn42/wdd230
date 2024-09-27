964879ce783d9239175b686e078d01d7c39728ec
hadamn42 <an.humes@gmail.com>
2024-09-27 07:23:19 -0600

    some Chamber and all team activity (night mode)

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,11 @@
+document.querySelector('#currentYear').textContent = new Date().getFullYear();
+
+document.querySelector('#lastModified').textContent = 'Last modified on ' + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,47 @@
 document.querySelector('#currentYear').textContent = new Date().getFullYear();
 
 document.querySelector('#lastModified').textContent = 'Last modified on ' + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌙")) {
+        document.body.style.setProperty('--body-bg', '#54708c')
+        document.body.style.setProperty('--main-bg', '#858dad')
+        document.body.style.setProperty('--heading-bg', '#133117')
+        document.body.style.setProperty('--text-color', 'white')
+        document.body.style.setProperty('--section-bg', '#0b5351')
+        document.body.style.setProperty('--nav-background-color', '#0e3c25')
+        document.body.style.setProperty('--nav-link-color', 'white')
+        document.body.style.setProperty('--card-link', 'yellow')
+        document.body.style.setProperty('--nav-active', 'rgba(255,255,255,0.15)') 
+        document.body.style.setProperty('--nav-hover', 'rgba(255,255,255,0.5)') 
+		modeButton.textContent = "🔆";
+	} else {
+        document.body.style.setProperty('--body-bg', '#A6979C')
+        document.body.style.setProperty('--main-bg', '#EEEEEE')
+        document.body.style.setProperty('--heading-bg', '#00a9a5')
+        document.body.style.setProperty('--text-color', 'black')
+        document.body.style.setProperty('--section-bg', '#F0F7F4')
+        document.body.style.setProperty('--nav-background-color', '#0b5351')
+        document.body.style.setProperty('--nav-link-color', '#F0F7F4')
+        document.body.style.setProperty('--nav-active', 'rgba(0,0,0,0.5)')
+        document.body.style.setProperty('--nav-hover', 'rgba(255,255,255,0.3)')       
+        document.body.style.setProperty('--card-link', 'blue')
+		modeButton.textContent = "🌙";
+	}
+});
+
+    
+
+
+
