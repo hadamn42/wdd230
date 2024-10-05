41dba74e7c2dec06ad38adb224a8db1c77653980
hadamn42 <an.humes@gmail.com>
2024-10-05 09:25:03 -0600

    pushing to test js in incognito

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,3 +9,24 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const visitDisplay = document.querySelector("#visit");
+const dateToday = new Date();
+const msToDays = 86400000;
+
+let visitDays = window.localStorage.getItem("numVis") || 0;
+
+let visitDiff = dateToday - visitDays;
+
+if (visitDays !== 0) {
+    if (visitDiff > msToDays) {
+		visitDisplay.textContent = `You last visited ${visitDiff/msToDays} days ago!`;
+	} else {
+		visitDisplay.textContent = "Back so soon! Awesome!";
+	}
+} else {
+    visitDisplay.textContent = "Welcome! Let us know if you have any questions";
+}
+
+
+localStorage.setItem("numVis", dateToday);
