d9e2c1dad93edec1c8f44710edb6aa6b8730ff7f
hadamn42 <an.humes@gmail.com>
2024-10-15 20:39:33 -0600

    Completed database activity

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -3,8 +3,34 @@ const cards = document.querySelector('#cards');
 
 async function getProphetData() {
     const response = await fetch(url);
-    const data = response.json();
+    const data = await response.json();
     console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let dob = document.createElement('p');
+        let pob = document.createElement('p');
+        let portrait = document.createElement('img');
+        let name = `${prophet.name} ${prophet.lastname}`;
+        fullName.textContent = name;
+        dob.textContent = `Date of birth: ${prophet.birthdate}`;
+        pob.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", name);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+        card.appendChild(fullName);
+        card.appendChild(dob);
+        card.appendChild(pob);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
 }
 
 getProphetData();
