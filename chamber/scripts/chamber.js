dc12a8702f24e720cd25fcc55eba93c6c1134d38
hadamn42 <an.humes@gmail.com>
2024-10-21 21:38:59 -0600

    Completed adding weather to the front page.

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -10,24 +10,3 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
-const visitDisplay = document.querySelector("#visit");
-const dateToday = new Date();
-const msToDays = 86400000;
-
-let visitDays = window.localStorage.getItem("numVis") || 0;
-
-
-let visitDiff = dateToday - visitDays;
-
-if (visitDays !== 0) {
-    if (visitDiff > msToDays) {
-		visitDisplay.textContent = `You last visited ${Math.floor(visitDiff/msToDays)} days ago!`;
-	} else {
-		visitDisplay.textContent = "Back so soon! Awesome!";
-	}
-} else {
-    visitDisplay.textContent = "Welcome! Let us know if you have any questions";
-}
-
-
-localStorage.setItem("numVis", dateToday);
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,14 +1,15 @@
-const currentTemp = document.querySelector("#weather");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
+// Current weather
+// Weather Info 
+const currentTemp = document.querySelector("#mainweather");
+
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.736&lon=-111.834&units=imperial&appid=cefb473376acd73ec09ccfe6712f37ad"
-// 41.736, -111.834
+
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -19,12 +20,73 @@ async function apiFetch() {
 }
 
     function displayResults(data) {
-        let desc = data.weather[0].description;
-        currentTemp.innerHTML = `${data.name}, ${data.sys.country}: ${data.main.temp}&deg; F - ${desc}`;
-        const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        weatherIcon.setAttribute('src', iconsrc);
-        weatherIcon.setAttribute('alt', desc);
+		let desc = document.createElement('p');
+		desc.classList.add('weathercon');
+		desc.textContent = data.weather[0].description;
+
+		let curTomp = document.createElement('p');
+		curTomp.innerHTML = `${data.main.temp}&deg; F`;
+
+		currentTemp.appendChild(desc);
+		currentTemp.appendChild(curTomp);
     }
 
 
 apiFetch();
+
+// 5-Day Forecast
+
+const forecasto = document.querySelector("#forecast");
+const url2 = "https://api.openweathermap.org/data/2.5/forecast?lat=41.736&lon=-111.834&units=imperial&appid=cefb473376acd73ec09ccfe6712f37ad";
+
+
+async function forecastFetch() {
+	try {
+		const response = await fetch(url2);
+		if (response.ok) {
+			const data = await response.json();
+			console.log(data);
+			displayForecast(data);
+		} else {
+			throw Error(await response.text());
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function displayForecast(data) {
+	const dayWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+	for (let i = 0; i < data.list.length; i+=8){
+		let sanction = document.createElement('section');
+		let y = i+6;
+		let z = i+1
+
+		let dateHd = document.createElement('h4');
+		let date = new Date(data.list[i].dt_txt);
+		dateHd.textContent = `${dayWeek[date.getDay()]}`;
+
+		let weathing = document.createElement('p');
+		weathing.classList.add('weathercon');
+		weathing.textContent = data.list[i].weather[0].description;
+
+		let minTemp = document.createElement('p');
+		minTemp.classList.add('tempmin');
+		minTemp.innerHTML = `${data.list[z].main.temp_min}&deg; F`;
+
+		let maxTemp = document.createElement('p');
+		maxTemp.classList.add('tempmax');
+		maxTemp.innerHTML = `${data.list[y].main.temp_max}&deg; F`;
+		
+		sanction.appendChild(dateHd);
+		sanction.appendChild(weathing);
+		sanction.appendChild(minTemp);
+		sanction.appendChild(maxTemp);
+		forecasto.appendChild(sanction);
+	}
+
+}
+
+
+forecastFetch();
